408ec6539ea12229c6cafb1cfd13d6162a532874
BenM4k <bennymakomo@gmail.com>
2024-07-29 14:14:14 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--fonts-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Care puls",
+  description: "Health care management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
